3f4fa5a1ae7ee284348c37a7cadfd625cd08b976
Komiyaki <kevinbeutler2003@gmail.com>
2026-07-07 16:39:28 +0900

    Now can reselect and added highlights

--- Scenes/Systems/game_manager.gd
+++ Scenes/Systems/game_manager.gd
@@ -15,11 +15,11 @@ RESTART,
 }
 
 var game_state: GameState = GameState.FIRST_LOAD:
-    set(value):
-        game_state = value
-        if(debug):
-            # ui_manager.display_game_state(GameState.keys()[game_state])
-            pass
+	set(value):
+		game_state = value
+		if(debug):
+			# ui_manager.display_game_state(GameState.keys()[game_state])
+			pass
 
 @export var carried_object_pool: ObjectPooler
 # @export var ui_manager
@@ -29,49 +29,49 @@ var game_state: GameState = GameState.FIRST_LOAD:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    game_state = GameState.FIRST_LOAD
-    _check_dependencies()
+	game_state = GameState.FIRST_LOAD
+	_check_dependencies()
 
 func _check_dependencies() -> void:
-    # if ui_manager == null:
-    #     push_error("No ui_manager set on %s" % name)
-    if carried_object_pool == null:
-        push_error("No carried_object_pool set on %s" % name)
-    if gm_round_timer == null:
-        push_error("No gm_round_timer set on %s" % name)
+	# if ui_manager == null:
+	#     push_error("No ui_manager set on %s" % name)
+	if carried_object_pool == null:
+		push_error("No carried_object_pool set on %s" % name)
+	if gm_round_timer == null:
+		push_error("No gm_round_timer set on %s" % name)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 
-    match game_state:
-        GameState.FIRST_LOAD:
-            game_state = GameState.FIRST_LOAD
-            print("%s performing first load" % name)
-            # for now dont do much, just start spawning
-            # might play some intro animation or something idk
-            game_state = GameState.ROUND_SWITCH
+	match game_state:
+		GameState.FIRST_LOAD:
+			game_state = GameState.FIRST_LOAD
+			print("%s performing first load" % name)
+			# for now dont do much, just start spawning
+			# might play some intro animation or something idk
+			game_state = GameState.ROUND_SWITCH
 
-        GameState.INTERROUND:
-            # allow shopping for new guys??? idk
-            pass
+		GameState.INTERROUND:
+			# allow shopping for new guys??? idk
+			pass
 
-        GameState.PAUSED:
-            # send signal to all subscribers that game is paused (on pause and unpause)
-            pass
+		GameState.PAUSED:
+			# send signal to all subscribers that game is paused (on pause and unpause)
+			pass
 
-        GameState.ROUND_SWITCH:
-            # increment round counter, load recipe data for next round, start interround timer
-            pass
+		GameState.ROUND_SWITCH:
+			# increment round counter, load recipe data for next round, start interround timer
+			pass
 
-        GameState.GAME_OVER:
-            # show stats
-            # allow restart, or exit to menu
-            pass
+		GameState.GAME_OVER:
+			# show stats
+			# allow restart, or exit to menu
+			pass
 
-        GameState.RESTART:
-            pass
+		GameState.RESTART:
+			pass
 
-        _:
-            push_error("Unexpected GameState value on %s" % name)
+		_:
+			push_error("Unexpected GameState value on %s" % name)
 
-    pass
+	pass
