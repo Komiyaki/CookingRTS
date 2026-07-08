be461c3777eba566745e89a4b4e3ddb44810bd40
Komiyaki <kevinbeutler2003@gmail.com>
2026-07-08 16:34:07 +0900

    Started working on movement
    Currently doing selected nodes moving to clicked position. Still need to make the selected units into a group and then move the group as a whole

--- Scenes/Game/test_1.gd
+++ Scenes/Game/test_1.gd
@@ -1,20 +1,32 @@
 extends Node2D
 
-func _ready() -> void:
-	$Selector.hide()
-	add_to_group("selectable-units")
+const MOVE_SPEED: float = 50.0
+var target_pos: Vector2
+@onready var selector = $Selector
 
+func _ready() -> void:
+	selector.hide()
+	add_to_group("selectable-units")
+	set_process(false)
+	
+func _process(delta: float) -> void:
+	global_position = global_position.move_toward(target_pos, MOVE_SPEED * delta)
+	if global_position.distance_to(target_pos) < 0.1:
+		global_position = target_pos
+		set_process(false)
+
+func move(_target_pos: Vector2):
+	target_pos = _target_pos
+	set_process(true)
 
 func is_in_selection_box(box: Rect2) -> bool:
 	var screen_pos := get_global_transform_with_canvas().origin
 	return box.has_point(screen_pos)
 
-
 func select() -> void:
-	$Selector.show()
+	selector.show()
 	add_to_group("selected-units")
 
-
 func deselect() -> void:
-	$Selector.hide()
+	selector.hide()
 	remove_from_group("selected-units")
--- Scenes/Game/unit_selector.gd
+++ Scenes/Game/unit_selector.gd
@@ -5,7 +5,7 @@ var drag_start: Vector2
 var select_box: Rect2
 
 const CLICK_THRESHOLD := 8.0
-const CLICK_SELECT_SIZE := Vector2(24, 24)
+const CLICK_SELECT_SIZE := Vector2(60, 60)
 
 
 func _input(e: InputEvent) -> void:
@@ -15,64 +15,40 @@ func _input(e: InputEvent) -> void:
 			drag_start = e.position
 			select_box = Rect2(drag_start, Vector2.ZERO)
 			queue_redraw()
-
 		else:
 			selecting = false
-
 			var additive: bool = Input.is_key_pressed(KEY_SHIFT)
-
 			if drag_start.distance_to(e.position) < CLICK_THRESHOLD:
-				select_box = Rect2(
-					e.position - CLICK_SELECT_SIZE / 2.0,
-					CLICK_SELECT_SIZE
-				)
+				select_box = Rect2(e.position - CLICK_SELECT_SIZE / 2.0,CLICK_SELECT_SIZE)
 			else:
 				var x_min = min(drag_start.x, e.position.x)
 				var y_min = min(drag_start.y, e.position.y)
-
-				select_box = Rect2(
-					x_min,
-					y_min,
-					max(drag_start.x, e.position.x) - x_min,
-					max(drag_start.y, e.position.y) - y_min
-				)
-
+				select_box = Rect2(x_min,y_min,max(drag_start.x, e.position.x) - x_min,max(drag_start.y, e.position.y) - y_min)
 			update_selected_units(additive)
 			queue_redraw()
-
 	elif selecting and e is InputEventMouseMotion:
 		var x_min = min(drag_start.x, e.position.x)
 		var y_min = min(drag_start.y, e.position.y)
-
-		select_box = Rect2(
-			x_min,
-			y_min,
-			max(drag_start.x, e.position.x) - x_min,
-			max(drag_start.y, e.position.y) - y_min
-		)
-
+		select_box = Rect2(x_min,y_min,max(drag_start.x, e.position.x) - x_min,max(drag_start.y, e.position.y) - y_min)
 		queue_redraw()
 
+func move_selected_units(pos: Vector2):
+	var units
 
 func _draw() -> void:
 	if not selecting:
 		return
-
 	if select_box.size.length() < CLICK_THRESHOLD:
 		return
-
 	draw_rect(select_box, Color("#00ff0066"))
 	draw_rect(select_box, Color("#00ff00"), false, 2.0)
-
 
 func update_selected_units(additive: bool) -> void:
 	if not additive:
 		clear_selected_units()
-
 	for unit in get_tree().get_nodes_in_group("selectable-units"):
 		if unit.is_in_selection_box(select_box):
 			unit.select()
-
 
 func clear_selected_units() -> void:
 	for unit in get_tree().get_nodes_in_group("selected-units"):
