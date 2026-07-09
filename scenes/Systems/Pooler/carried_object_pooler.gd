ad1dd8f4a427c7b8109c2ebf2ae9ca5e600272c4
FurnitureSet <65529437+FurnitureSet@users.noreply.github.com>
2026-07-09 15:52:19 +0900

    Fix merge conflict on id_dict

--- scenes/Systems/Pooler/carried_object_pooler.gd
+++ scenes/Systems/Pooler/carried_object_pooler.gd
@@ -9,15 +9,15 @@ class_name CarriedObjectPooler
 @export var sprite_dict: Dictionary
 
 func _ready() -> void:
-	_check_dependencies()
+    _check_dependencies()
 
-	super()
+    super()
 
     _build_sprite_dict()
 
 func _check_dependencies() -> void:
-	if game_manager == null:
-		push_error("No game_manager set on %s" % name)
+    if game_manager == null:
+        push_error("No game_manager set on %s" % name)
 
 func _build_sprite_dict() -> void:
     sprite_dict = Dictionary()
@@ -27,18 +27,11 @@ func _build_sprite_dict() -> void:
         sprite_dict.set(key, load(sprite_folder + IDDictionary.id_dict.get(key).get(sprite_path_key) + sprite_type))
 
 func spawn_carried_object(location: Vector2, id: int) -> CarriedObject:
-	var object: CarriedObject =  super.spawn_object(location)
+    var object: CarriedObject =  super.spawn_object(location)
 
-<<<<<<< Updated upstream
     # Get object from super() and set sprite, id manually
     assert(object is CarriedObject)
     object.id = id
     object.sprite.texture = sprite_dict.get(id)
-=======
-	# Get object from super() and set sprite, id manually
-	assert(object is CarriedObject)
-	object.id = id
-	object.sprite.texture = load(game_manager.carried_object_texture)
->>>>>>> Stashed changes
 
-	return object
+    return object
--- scenes/Systems/id_dictionary.gd
+++ scenes/Systems/id_dictionary.gd
@@ -1,7 +1,6 @@
 class_name IDDictionary
 
 const id_dict: Dictionary = {
-<<<<<<< Updated upstream
     # Plate
     0b000000: { # 0
         "name": "Plate",
@@ -122,107 +121,4 @@ const id_dict: Dictionary = {
         "timer_value": 0,
         "sprite_name": "default",
     },
-=======
-	# Plate
-	0b000000: { # 0
-		"name": "Plate",
-		"carry_value": 4,
-		"timer_value": 0
-	},
-	# Carbs
-	0b001000: { # 8
-		"name": "Rice",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b001001: { # 9
-		"name": "Noodle",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	# Protein
-	0b010000: { # 16
-		"name": "Chicken",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b010001: { # 17
-		"name": "Beef",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b010010: { # 18
-		"name": "Pork",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b010011: { # 19
-		"name": "Shrimp",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	# Vegetable
-	0b011000: { # 24
-		"name": "Cabbage",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b011001: { # 25
-		"name": "Chives",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b011010: { # 26
-		"name": "Carrot",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b011011: { # 27
-		"name": "Bok Choy",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b011100: { # 28
-		"name": "Bean Sprout",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b011101: { # 29
-		"name": "Pepper",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	# Sauce
-	0b101000: { # 40
-		"name": "Soy",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b101001: { # 41
-		"name": "Oyster",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b101010: { # 42
-		"name": "Worcestershire",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	# Seasoning
-	0b110000: { # 48
-		"name": "Salt",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b110001: { # 49
-		"name": "Pepper",
-		"carry_value": 3,
-		"timer_value": 0
-	},
-	0b110010: { # 50
-		"name": "MSG",
-		"carry_value": 3,
-		"timer_value": 0
-	},
->>>>>>> Stashed changes
 }
