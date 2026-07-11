b840e8ea8adfd999d1bc12effe7228625fbec52f
FurnitureSet <65529437+alanbthomson@users.noreply.github.com>
2026-07-11 15:46:36 +0900

    Recipe Dictionary base

---
+++ scenes/Data/recipe_dictionary.gd
@@ -0,0 +1,15 @@
+class_name RecipeDictionary
+
+const recipe_dict: Dictionary = {
+    1: {
+        "recipe_name": "Shrimp Fried Rice",
+        "ingredients" : {
+            0b001000: 2, # Shrimp
+            0b010011: 2, # Rice
+            0b011000: 2, # Cabbage
+            0b011100: 2, # Bean Sprout
+            0b100000: 1, # Soy
+            0b101010: 1, # MSG
+        }
+    }
+}
