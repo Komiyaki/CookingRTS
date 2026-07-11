080934e6c3dc8428392f0cd463949b68c54bfff8
Komiyaki <kevinbeutler2003@gmail.com>
2026-07-11 15:42:44 +0900

    move queue added and working

--- Scenes/Systems/little_guy_order.gd
+++ Scenes/Systems/little_guy_order.gd
@@ -2,31 +2,24 @@ extends RefCounted
 class_name LittleGuyOrder
 
 enum ActionType {
-MOVE,
-PICKUP,
-PUTDOWN,
-INTERACT
+    MOVE,
+    PICKUP,
+    PUTDOWN,
+    INTERACT
 }
 
 var action_type: ActionType
-var target_objective: Objective = null
 var target_position: Vector2 = Vector2.ZERO
+var target_objective: Node2D = null
 var item_id: int = -1
 
-
-func _init(
-    _action_type: ActionType,
-    _target_objective: Objective = null,
-    _target_position: Vector2 = Vector2.ZERO,
-    _item_id: int = -1
-) -> void:
-    action_type = _action_type
-    target_objective = _target_objective
-    target_position = _target_position
-    item_id = _item_id
-
+static func make_move_order(pos: Vector2) -> LittleGuyOrder:
+    var order := LittleGuyOrder.new()
+    order.action_type = ActionType.MOVE
+    order.target_position = pos
+    return order
 
 func get_target_position() -> Vector2:
     if target_objective != null and is_instance_valid(target_objective):
-        return target_objective.get_target_position()
+        return target_objective.global_position
     return target_position
